6711cd4dc214877e826967a6457cd9378a281b38
YumeMichi <do4suki@gmail.com>
2020-01-02 12:10:43 +0800

    Equuleus: Enable double tap to wake feature
    Change-Id: I3d9de7d4e40d550459138dec5ba1bb5c68cb2b8e
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- device.mk
+++ device.mk
@@ -19,8 +19,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
-
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-pa
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
