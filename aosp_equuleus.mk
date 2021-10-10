0f25c1218c20a1438cf39972024f6d91d30c7158
Jwhan <im.jianweihan@gmail.com>
2021-10-10 10:32:19 +0800

    equuleus: Redfin RQ3A.211001.001.A1/7641976

--- aosp_equuleus.mk
+++ aosp_equuleus.mk
@@ -28,10 +28,10 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 8 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210905.001.A1/7511028:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210905.001.A1 7511028 release-keys" \
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.211001.001.A1 7641976 release-keys" \
     PRODUCT_NAME="equuleus"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
