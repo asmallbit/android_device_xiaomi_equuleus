4d645b5335140231516d4fa565ebd3e2d0aefe5a
jwhan <im.jianweihan@gmail.com>
2021-11-14 14:09:37 +0000

    Revert "equuleus: Update redfin fingerprint to November"
    This reverts commit dfbe16fe6a69394e4852bcfe5e4af4cc2fc4c1d0.

--- aosp_equuleus.mk
+++ aosp_equuleus.mk
@@ -26,10 +26,10 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 8 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/redfin/redfin:12/SP1A.211105.003/7757856:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 12 SP1A.211105.003 7757856 release-keys" \
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.211001.001.A1 7641976 release-keys" \
     PRODUCT_NAME="equuleus"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
